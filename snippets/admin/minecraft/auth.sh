10bb898e49d57b49d3001c6f4b8b7325811abcec
Scott Blomquist <github@scott.blomqui.st>
2011-08-22 14:38:20 -0700

    Auto-execute action using @reboot in crontab.
    Also updated auth.sh to not use $USER, which I shouldn't be overwriting in a script.

--- snippets/admin/minecraft/auth.sh
+++ snippets/admin/minecraft/auth.sh
@@ -1,1 +1,1 @@
-curl -I -H "X-Auth-User: $1" -H "X-Auth-Key: $2" https://auth.api.rackspacecloud.com/v1.0
+curl -I -H "X-Auth-User: $RSC_USER" -H "X-Auth-Key: $RSC_KEY" https://auth.api.rackspacecloud.com/v1.0
