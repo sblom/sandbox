da05bf0c3c541cba908836a087919ccb316a89a6
"Scott Blomquist" <github@scott.blomqui.st>
2012-01-22 15:54:11 -0800

    Add a quick try run of Microsoft P&P's Unity framework.

---
+++ try/VS/Unity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace Unity
+{
+  interface IFoo
+  {
+    string Name { get; set; }
+  }
+  class Foo: IFoo
+  {
+    private string name_;
+    public string Name {
+      get {
+        return name_;
+      }
+      set {
+        name_ = value;
+      }
+    }
+    public Foo(string name)
+    {
+      name_ = name;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var container = new UnityContainer();
+      container.RegisterType<IFoo,Foo>(new ContainerControlledLifetimeManager(), new InjectionConstructor("Scott"));
+      IFoo myFoo = container.Resolve<IFoo>();
+      IFoo myFoo2 = container.Resolve<IFoo>();
+      Console.WriteLine(myFoo.Name);
+      Console.WriteLine(Object.ReferenceEquals(myFoo, myFoo2));
+    }
+  }
+}
